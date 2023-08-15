c0692f74de7cd08fc3484a6f58f9cb350b58e21e
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-08-15 14:53:36 +0200

    4 endpoints, one per component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RetentionComponent } from './frameworkanalysis/retention/retention.component';
+import { InterestComponent } from './frameworkanalysis/interest/interest.component';
+import { UsageComponent } from './frameworkanalysis/usage/usage.component';
+import { AwarenessComponent } from './frameworkanalysis/awareness/awareness.component';
 
 const routes: Routes = [
-  { path: '', component: RetentionComponent },
+  { path: '', component: RetentionComponent },  
+  { path: 'interest', component: InterestComponent },
+  { path: 'usage', component: UsageComponent },
+  { path: 'awareness', component: AwarenessComponent },
 ];
 
 @NgModule({
--- src/app/frameworkanalysis/frameworkanalysis.module.ts
+++ src/app/frameworkanalysis/frameworkanalysis.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RetentionComponent } from './retention/retention.component';
-
-
+import { InterestComponent } from './interest/interest.component';
+import { UsageComponent } from './usage/usage.component';
+import { AwarenessComponent } from './awareness/awareness.component';
 
 @NgModule({
   declarations: [
-    RetentionComponent
+    RetentionComponent,
+    InterestComponent,
+    UsageComponent,
+    AwarenessComponent
   ],
   imports: [
     CommonModule
