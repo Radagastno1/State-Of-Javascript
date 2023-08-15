ee9d1b4558e84bde9bda15af0595bf385cba21ad
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-08-15 21:56:34 +0200

    årtal och retention procent

--- src/app/frameworkanalysis/retention/retention.component.ts
+++ src/app/frameworkanalysis/retention/retention.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { frameworks } from 'data';
+import { Framework, frameworks } from 'data';
 
 @Component({
   selector: 'app-retention',
@@ -9,4 +9,15 @@ import { frameworks } from 'data';
 export class RetentionComponent {
   retentionData = frameworks;
    years = frameworks[0].surveys.map(survey => survey.year);
+
+   getSurveyOfYear(year:number, framework:Framework){
+    const foundFramework = frameworks.find(f => f.name == framework.name);
+    const surveyAtSpecificYear = foundFramework?.surveys.find(s => s.year == year);
+    if(surveyAtSpecificYear){
+      return surveyAtSpecificYear.retention;
+    }
+    else{
+      return null;
+    }
+   }
 }
